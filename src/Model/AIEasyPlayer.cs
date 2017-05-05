dfb142c075d4c6caca500444a33c1bda56a8668b
Jay Kaswan <08jk11@gmail.com>
2017-05-06 04:52:21 +1000

    AIEasyPlayer

--- src/Model/AIEasyPlayer.cs
+++ src/Model/AIEasyPlayer.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// The AIEAsyPlayer is a kind of AIPlayer which hits just randomly.
+/// </summary>
+
+public class AIEasyPlayer : AIPlayers
+{
+	
+}
